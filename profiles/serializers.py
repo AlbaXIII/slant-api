f019945e39965fc8fc02eb8d2f9e56a5a48e28bc
AlbaXIII <alex.jbaker20@googlemail.com>
2025-04-19 10:36:15 +0100

    feat - add auth points for user log in/ou, defined profile owner permissions

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -4,9 +4,15 @@ from .models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
 
     class Meta:
         model = Profile
         fields = [
-            'id', 'owner', 'name', 'bio', 'created_on', 'updated_on'
+            'id', 'owner', 'name', 'bio',
+            'created_on', 'updated_on', 'is_owner'
         ]
