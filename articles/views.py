417c3444ad3e53bf72c5f706c432897ee7d2e06c
AlbaXIII <alex.jbaker20@googlemail.com>
2025-05-08 16:32:24 +0100

    feat - add search & filter fields to article view

--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,6 @@
 from django.db.models import Count
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Article
 from .serializers import ArticleSerializer
 from slant_api.permissions import ReadOnlyIfNotOwner
@@ -14,6 +15,31 @@ class ArticleList(generics.ListCreateAPIView):
         favourites_count=Count('favourites', distinct=True),
         comments_count=Count('comment', distinct=True)
     ).order_by('-created_on')
+
+    filter_backends = [
+        filters.SearchFilter,
+        filters.OrderingFilter,
+        DjangoFilterBackend,
+    ]
+
+    filterset_fields = [
+        'owner__article__owner__profile',
+        'favourites__owner__profile',
+        'owner__comment__owner__profile',
+        'owner__profile',
+    ]
+
+    search_fields = [
+        'owner__username',
+        'title',
+        'subject',
+    ]
+
+    ordering_fields = [
+        'favourites_count',
+        'comments_count',
+        'favourites__created_at',
+    ]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
