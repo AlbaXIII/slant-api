91dc8c4784ab4a657a44e14ccd1712f0f1a44b4c
AlbaXIII <alex.jbaker20@googlemail.com>
2025-05-10 19:19:14 +0100

    feat - add filter to comments associated to specific post

--- comments/views.py
+++ comments/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics, permissions
 from slant_api.permissions import ReadOnlyIfNotOwner
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Comment
 from .serializers import CommentSerializer, CommentDetailSerializer
 
@@ -8,6 +9,9 @@ class CommentList(generics.ListCreateAPIView):
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Comment.objects.all()
+
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['article']
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
