c295173205e7005543dbad3e71d6b06df3d0cf6a
AlbaXIII <alex.jbaker20@googlemail.com>
2025-05-12 08:56:31 +0100

    maint - add pagination, root route and datetime formatting for deploy

--- slant_api/settings.py
+++ slant_api/settings.py
@@ -79,14 +79,22 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication'
         if 'DEV' in os.environ
         else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
-    )]
+    )],
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'DATETIME_FORMAT': '%d %b %Y'
 }
+
+if 'DEV' not in os.environ:
+    REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
+        'rest_framework.renderers.JSONRenderer',
+    ]
 
 REST_USE_JWT = True
 JWT_AUTH_COOKIE = 'my-app-auth'
 JWT_AUTH_SECURE = True
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
-
 
 REST_AUTH_SERIALIZERS = {'USER_DETAILS_SERIALIZER': 'slant_api.serializers.CurrentUserSerializer'}
 
