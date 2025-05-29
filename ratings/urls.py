e99cf204b3e505d37a0262587240ce013926c5b8
AlbaXIII <alex.jbaker20@googlemail.com>
2025-05-29 08:44:25 +0100

    maint - fix typo in rating urlpattern

--- ratings/urls.py
+++ ratings/urls.py
@@ -3,7 +3,7 @@ from ratings import views
 
 urlpatterns = [
     path('ratings/', views.RatingList.as_view()),
-    path('ratings/<int:pk>/', views.RatingDetail.as_view()),
+    path('ratings/<int:pk>/', views.RatingDetailView.as_view()),
     path('articles/<int:article_id>/rating-stats/',
          views.article_rating_stats, name='article-rating-stats'),
 ]
